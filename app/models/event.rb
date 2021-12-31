39a268996fe4133354d388072110e75650157f40
kirby_lam <kirbylam16@gmail.com>
2021-12-30 16:55:59 -0800

    Refactored index page so that query logic is in methods in the model instead of the index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,16 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :location, presence: true
-  validates :date, presence: true 
+  validates :date, presence: true
+
+  scope :past, -> { where("date <= ?", Date.today) }
+  scope :future, -> { where("date > ?", Date.today) }
+
+  def self.past_events
+    Event.past
+  end
+
+  def self.future_events
+    Event.future
+  end
 end
